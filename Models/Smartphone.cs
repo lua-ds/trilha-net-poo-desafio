8859d3eb1bcb0b1e5f34ed26ec0033332a4a66cb
luâ ferreira dos santos <luanfds@outlook.com.br>
2024-12-02 23:03:25 -0300

    Resolução do Desafio DIO POO

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,13 +3,23 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+
+        private string Modelo { get; set; }
+
+        private string Imei { get; set; }
+
+        private int Memoria { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+            
         }
+
 
         public void Ligar()
         {
@@ -22,5 +32,9 @@ namespace DesafioPOO.Models
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+                 
+        
     }
+
+
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("123456","15","11111", 5 );
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("whatsApp");
+
+Console.WriteLine("\n");
+
+Nokia nokia = new Nokia("987456321","3300","22222",65);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
